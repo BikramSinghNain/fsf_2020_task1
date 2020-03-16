c7f63afe3ea881cc9e364217362efd025d201e0d
BikramSinghNain <61709677+BikramSinghNain@users.noreply.github.com>
2020-03-16 20:04:33 +0530

    This is my drupal 7 JAVA script.

---
+++ Drupal 7/js/display.js
@@ -0,0 +1,15 @@
+(function ($) {
+
+    $(function () {
+        $.fn.displayMarks = function (data) {
+            document.getElementById("display_marks").innerHTML = data;
+        }
+
+        $.fn.diplaySave = function (data) {
+        
+            document.getElementById('display_files').innerHTML = data;
+        }
+        
+    });
+
+})(jQuery);
